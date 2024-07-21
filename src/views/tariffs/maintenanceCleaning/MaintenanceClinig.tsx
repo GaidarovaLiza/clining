75e6c7357a6fd264ea991feb33012a2390537ec5
KilinaEkaterina <57572971+Ekaterina31415@users.noreply.github.com>
2024-07-21 17:29:32 +0600

    updated data (#17)

--- src/views/tariffs/maintenanceCleaning/MaintenanceClinig.tsx
+++ src/views/tariffs/maintenanceCleaning/MaintenanceClinig.tsx
@@ -26,32 +26,32 @@ export const MaintenanceCleaning = () => {
       {
         id: '1',
         room: '1 комната (студия)',
-        price: '280-315 р',
+        price: '85 р',
       },
       {
         id: '2',
         room: '1 комната (кухня + комната)',
-        price: '300-320 р',
+        price: '90-100 р',
       },
       {
         id: '3',
         room: '2 комнаты (до 45 кв. м.)',
-        price: '320-350 р',
+        price: '110 р',
       },
       {
         id: '4',
         room: '2 комнаты (45+ кв. м.)',
-        price: '330-360 р',
+        price: '130 р',
       },
       {
         id: '5',
         room: '3 комнаты (до 60 кв. м.)',
-        price: '350-400 р',
+        price: '150 р',
       },
       {
         id: '6',
         room: '3 комнаты (70+ кв. м.)',
-        price: '450+ р',
+        price: '160-190 р',
       },
     ];
   }, []);
@@ -61,7 +61,7 @@ export const MaintenanceCleaning = () => {
       <div ref={maintenanceCleaningRef} className={style.textWrapper}>
         <Typography variant="h1">Поддерживающая уборка</Typography>
         <Typography className={style.subTitle} variant="h2">
-          Влажная и сухая уборка квартиры, бытовой техники, шкафов, лоджии
+          Сухая и влажная уборка поверхностей
         </Typography>
       </div>
       <Table<RowType> initialColumns={initialColumns} rows={data} />
