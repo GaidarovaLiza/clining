2a105b69a9a0f177a87158fbf4a19abeab07d7eb
KilinaEkaterina <57572971+Ekaterina31415@users.noreply.github.com>
2024-07-21 15:13:13 +0600

    added maintenance cleaning table (#16)

--- src/views/tariffs/Tariffs.tsx
+++ src/views/tariffs/Tariffs.tsx
@@ -1,5 +1,6 @@
 import { AdditionalItems } from './additionalItems/AdditionalItems';
 import { GeneralCleaning } from './generalClining/GeneralClinig';
+import { MaintenanceCleaning } from './maintenanceCleaning/MaintenanceClinig';
 
 import style from './Tariffs.module.scss';
 
@@ -8,6 +9,9 @@ export const Tariffs = () => {
     <div className={style.wrapper}>
       <div className={style.wrapperTables}>
         <GeneralCleaning />
+      </div>
+      <div className={style.wrapperTables}>
+        <MaintenanceCleaning />
       </div>
       <AdditionalItems />
     </div>
